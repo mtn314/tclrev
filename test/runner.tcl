ef41ab623be96071615bc0dcd9b716ec7ac05432
mtn314 <mtn314@outlook.com>
2016-02-29 20:06:28 +0000

    fixing test runner to pick up subdirectories for unit testing

--- test/runner.tcl
+++ test/runner.tcl
@@ -1,8 +1,13 @@
 #
 package require tcltest
 
-# If the app has more directories, we need to execute runAllTests for each of those
-foreach dir [list [file dirname [info script]]] {
+set test_root_dir [file dirname [info script]]
+set dirs $test_root_dir
+# Find all directories in the test directory
+lappend dirs [glob -directory $test_root_dir -types d *]
+
+# Execute runAllTests for each of the directories that contain tcl tests
+foreach dir $dirs {
     puts ""
     puts "======== Directory: $dir ========"
     ::tcltest::configure -testdir $dir
