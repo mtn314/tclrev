f7081270a0992c6323f883c589daf4b8cd3c5590
mtn314 <mtn314@outlook.com>
2016-03-05 12:01:02 +0000

    expanding test subdirs correctly

--- test/runner.tcl
+++ test/runner.tcl
@@ -4,7 +4,7 @@ package require tcltest
 set test_root_dir [file dirname [info script]]
 set dirs $test_root_dir
 # Find all directories in the test directory
-lappend dirs [glob -directory $test_root_dir -types d *]
+lappend dirs {*}[glob -directory $test_root_dir -types d *]
 
 # Execute runAllTests for each of the directories that contain tcl tests
 foreach dir $dirs {
